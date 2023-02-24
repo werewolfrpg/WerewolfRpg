38af75af4230ff590e2d772dedae3d29756c5d47
Aesten <liliasfhaleaf@gmail.com>
2023-02-24 18:36:03 +0100

    removed unused classes

--- src/main/java/net/aesten/wwrpg/tracker/Event.java
+++
@@ -1,7 +0,0 @@
-package net.aesten.wwrpg.tracker;
-
-import java.sql.Timestamp;
-
-public abstract class Event {
-    private Timestamp timestamp;
-}
--- src/main/java/net/aesten/wwrpg/tracker/events/SimpleItemUseEvent.java
+++
@@ -1,7 +0,0 @@
-package net.aesten.wwrpg.tracker.events;
-
-import net.aesten.wwrpg.tracker.Event;
-
-public class SimpleItemUseEvent extends Event {
-
-}
