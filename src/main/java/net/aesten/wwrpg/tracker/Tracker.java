a9f0d828a24d7fa97033c27fde56c8f1e750c5fa
Aesten <liliasfhaleaf@gmail.com>
2023-03-13 20:12:19 +0100

    removed json functionalities

--- src/main/java/net/aesten/wwrpg/tracker/Tracker.java
+++ src/main/java/net/aesten/wwrpg/tracker/Tracker.java
@@ -1,15 +1,9 @@
 package net.aesten.wwrpg.tracker;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import net.aesten.wwrpg.WerewolfRpg;
 import net.aesten.wwrpg.data.Role;
 import net.aesten.wwrpg.utilities.WerewolfUtil;
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.*;
 
 public class Tracker {
@@ -45,38 +39,5 @@ public class Tracker {
                     }
                 }
             );
-    }
-
-    private static String getJson(PlayerStats stats) {
-        ObjectMapper mapper = new ObjectMapper();
-        try {
-            return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(stats);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public void sendJsonData() {
-        String root = Bukkit.getServer().getWorldContainer().getAbsolutePath();
-        String path = root + File.separator + "stats";
-        if (new File(path).mkdir()) {
-            WerewolfRpg.logConsole("Initialized stats folder");
-        }
-        String matchId = UUID.randomUUID().toString();
-        playerStats.forEach((playerId, stats) -> {
-            String fullPath = path + File.separator + matchId + File.separator + playerId.toString() + ".json";
-            saveJsonData(fullPath, getJson(stats));
-        });
-        playerStats.values().stream().map(Tracker::getJson).forEach(json -> saveJsonData(json, json));
-    }
-
-    private static void saveJsonData(String fullPath, String json) {
-        try {
-            FileWriter writer = new FileWriter(fullPath);
-            writer.write(json);
-            writer.close();
-        } catch (IOException e) {
-            WerewolfRpg.logConsole("Saving stats as json failed for: " + fullPath);
-        }
     }
 }
