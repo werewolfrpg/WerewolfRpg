9947be48a0856ecfc1702d1402f24110e40a1ddf
Aesten <liliasfhaleaf@gmail.com>
2023-04-01 01:50:34 +0200

    Completed bot

--- src/main/java/net/aesten/werewolfbot/WerewolfBot.java
+++ src/main/java/net/aesten/werewolfbot/WerewolfBot.java
@@ -1,6 +1,6 @@
 package net.aesten.werewolfbot;
 
-import net.aesten.werewolfbot.commands.GlobalCommands;
+import net.aesten.werewolfbot.commands.DiscordCommand;
 import net.aesten.werewolfbot.commands.CommandManager;
 import net.aesten.werewolfdb.QueryManager;
 import net.dv8tion.jda.api.JDA;
@@ -8,6 +8,8 @@ import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.entities.channel.concrete.VoiceChannel;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 
 import java.util.ArrayList;
@@ -16,6 +18,7 @@ import java.util.List;
 public class WerewolfBot {
     private final List<Guild> subscribedGuilds;
     private final JDA jda;
+    private Session currentSession;
 
     public WerewolfBot(String botToken) {
         JDABuilder builder = JDABuilder.createDefault(botToken);
@@ -23,9 +26,9 @@ public class WerewolfBot {
         builder.setActivity(Activity.playing("WerewolfRPG"));
         builder.enableIntents(List.of(GatewayIntent.GUILD_MESSAGES, GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MEMBERS));
         jda = builder.build();
-        jda.addEventListener(new CommandManager());
-        jda.updateCommands().addCommands(GlobalCommands.subscribeCommand).queue();
-
+        CommandManager manager = new CommandManager();
+        jda.addEventListener(manager);
+        jda.updateCommands().addCommands(manager.getCommands().stream().map(DiscordCommand::getCommand).toList()).queue();
         subscribedGuilds = new ArrayList<>(QueryManager.requestGuildIdList().stream().map(jda::getGuildById).toList());
     }
 
@@ -35,5 +38,22 @@ public class WerewolfBot {
 
     public List<Guild> getSubscribedGuilds() {
         return subscribedGuilds;
+    }
+
+    public boolean isSubscribed(Guild guild) {
+        return subscribedGuilds.contains(guild);
+    }
+
+    public void newSession(VoiceChannel vc, TextChannel lc) {
+        currentSession = new Session(vc, lc);
+    }
+
+    public void endSession() {
+        currentSession.terminate();
+        currentSession = null;
+    }
+
+    public Session getCurrentSession() {
+        return currentSession;
     }
 }
